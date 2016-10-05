085d10003fe0e8b8a58fadc6df32e1ac41b3a04a
jordan <jordan.lefort@laposte.net>
2016-10-05 11:34:12 +0200

    call player

--- app.js
+++ app.js
@@ -5,38 +5,54 @@ var app = {
 	playerOne : 1,
 	playerTwo : 0,
 	tr : ['.tr_1','.tr_2','.tr_3','.tr_4','.tr_5','.tr_6'],
-	tdCase : " .case",
 	changeColor : function(data,color,len){
 
-			if($('.tr_6' + app.tdCase + data).css('backgroundColor') === "rgb(128, 128, 128)" ){
+			if($('.tr_6' + " .case" + data).css('backgroundColor') === "rgb(128, 128, 128)" ){
 
-				$('.tr_6' + app.tdCase + data).css('backgroundColor',color);
+				$('.tr_6' + " .case" + data).css('backgroundColor',color);
 
 			}
 			else{
 				for(var i = len; i >= 0 ;i-- ){
-					var haveFont = $(app.tr[i] + app.tdCase + data).css('backgroundColor');
+					var haveFont = $(app.tr[i] + " .case" + data).css('backgroundColor');
 										
 					if(haveFont === "rgb(128, 128, 128)" ){
-						$(app.tr[i] + app.tdCase + data).css('backgroundColor',color);
+						$(app.tr[i] + " .case" + data).css('backgroundColor',color);
 						break;
 					}
 				}
 			}
 		},
 		
+	callPlayer : function(player){
+			
+		var call = $('.call').html("joueur " + player + " à toi de jouer");
+		return call;
+
+	},
+
+	rollPlayer : function(){
+		if(app.playerOne === 1){
+			app.playerOne-- ;
+			app.playerTwo++ ;
+			app.callPlayer(2);
+		}else{
+			app.playerOne++ ;
+			app.playerTwo-- ;
+			app.callPlayer(1);
+		}
+	},
 
 	unit : function(){
+	
 		var len = app['tr'].length;
-
+		app.callPlayer(1);
 		$('button').click(function(){
 			if(app.playerOne === 1 ){
-				app.playerOne = 0 ;
-				app.playerTwo = 1 ;
+				app.rollPlayer();
 				app.changeColor($(this).data('y'),"red",len);
 			}else{
-				app.playerOne = 1 ;
-				app.playerTwo = 0 ;
+				app.rollPlayer();
 				app.changeColor($(this).data('y'),"yellow",len);
 			}	
 		});
