784bccb631d0d709df7cb2ce63ff3dde3c25ce57
jordan <jordan.lefort@laposte.net>
2016-10-05 08:42:23 +0200

    refact

--- app.js
+++ app.js
@@ -5,20 +5,19 @@ var app = {
 	playerOne : 1,
 	playerTwo : 0,
 	tr : ['.tr_1','.tr_2','.tr_3','.tr_4','.tr_5','.tr_6'],
-	tdCase : " .case",
 	changeColor : function(data,color,len){
 
-			if($('.tr_6' + app.tdCase + data).css('backgroundColor') === "rgb(128, 128, 128)" ){
+			if($('.tr_6' + " .case" + data).css('backgroundColor') === "rgb(128, 128, 128)" ){
 
-				$('.tr_6' + app.tdCase + data).css('backgroundColor',color);
+				$('.tr_6' + " .case" + data).css('backgroundColor',color);
 
 			}
 			else{
 				for(var i = len; i >= 0 ;i-- ){
-					var haveFont = $(app.tr[i] + app.tdCase + data).css('backgroundColor');
+					var haveFont = $(app.tr[i] + " .case" + data).css('backgroundColor');
 										
 					if(haveFont === "rgb(128, 128, 128)" ){
-						$(app.tr[i] + app.tdCase + data).css('backgroundColor',color);
+						$(app.tr[i] + " .case" + data).css('backgroundColor',color);
 						break;
 					}
 				}
