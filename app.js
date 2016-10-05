92ecb43b7fabc20cadbc685b2b250139e32aa378
Amine EL G <aelghayate@simplon.co>
2016-10-05 10:19:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,65 +1,50 @@
 (function(){
 
-var app = {
-	
-	playerOne : 1,
-	playerTwo : 0,
-	tr : ['.tr_1','.tr_2','.tr_3','.tr_4','.tr_5','.tr_6'],
-	changeColor : function(data,color,len){
-		this.rollPlayer();
-		if($('.tr_6' + " .case" + data).css('backgroundColor') === "rgb(128, 128, 128)" ){
-			$('.tr_6' + " .case" + data).css('backgroundColor',color);
-		}
-		else{
+	var app = {	
+		turn:0,
+		tr : ['.tr_1','.tr_2','.tr_3','.tr_4','.tr_5','.tr_6'],
+		init : function(){
+
+			var len = this['tr'].length;
+			this.callPlayer(1);
+			var color;
+			$('button').click(function(){
+				var color = (!app.turn) ? "red" : "yellow";
+				app.changeColor($(this).data('y'), color, len);
+			});
+
+		},
+		changeColor : function(data,color,len){
+			this.rollPlayer();
+			//mettre en cache les selecteurs
+			var line = $(".tr_6 .case" + data);
+			if(line.css('backgroundColor') === "rgb(128, 128, 128)" ){
+				line.css('backgroundColor',color);
+				return;
+			} 
+
 			for(var i = len; i >= 0 ;i-- ){
 				var haveFont = $(this.tr[i] + " .case" + data).css('backgroundColor');				
 				if(haveFont === "rgb(128, 128, 128)" ){
 					$(this.tr[i] + " .case" + data).css('backgroundColor',color);
-					break;
+					return;
 				}
 			}
-		}
-	},
-		
-	callPlayer : function(player){
-		var call = $('.call').html("joueur " + player + " à toi de jouer");
-		return call;
-	},
 
-	rollPlayer : function(){
-		if(app.playerOne === 1){
-			this.playerOne-- ;
-			this.playerTwo++ ;
-			this.callPlayer(2);
-		}else{
-			this.playerOne++ ;
-			this.playerTwo-- ;
-			this.callPlayer(1);
-		}
-	},
+		},
 
+		callPlayer : function(player){
+			player = (player) ? 1 : 2;
+			var call = $('.call').html("joueur " + player + " à toi de jouer");
+			return call;
+		},
 
-	unit : function(){
-	
-		var len = this['tr'].length;
-		this.callPlayer(1);
-		$('button').click(function(){
-			if(app.playerOne === 1 ){
-				app.changeColor($(this).data('y'),"red",len);
-			}else{
-				app.changeColor($(this).data('y'),"yellow",len);
-			}	
-		});
+		rollPlayer : function(){
+			this.turn = !this.turn;
+			this.callPlayer(this.turn);
+		},
+	};
 
-	}
-		
-
-}
-
-
-app.unit();
-	
-
-
+	app.init();	
 })();
 
